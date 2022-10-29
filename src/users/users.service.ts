e7a7ee56685ecc3b0fe044eda23309a5f13da0c1
zullapo <pcluz62@gmail.com>
2022-10-29 09:36:11 -0300

    Módulo users

---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './repositories/users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return await this.usersRepository.create(createUserDto);
+  }
+
+  async findAll() {
+    return await this.usersRepository.findAll();
+  }
+
+  async findOne(id: number) {
+    return await this.usersRepository.findOne(id);
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.usersRepository.update(id, updateUserDto);
+  }
+
+  async remove(id: number) {
+    return await this.usersRepository.remove(id);
+  }
+}
